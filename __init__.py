37920c105b8dfb0efe52f92360c2cdcafacda830
dg <>
2021-06-26 11:18:12 +0200

    preparation for view matrix intersection

--- __init__.py
+++ __init__.py
@@ -18,6 +18,7 @@
 
 import bpy
 import numpy
+import mathutils
 from mathutils import Vector 
 from bpy.types import AddonPreferences
 from bpy.types import Operator 
@@ -77,60 +78,92 @@ def obj_callback(obj):
     pref = bpy.context.preferences.addons[__package__.split(".")[0]].preferences
     objPosition = []
     objDimension = []
+    
+    calc_view_matrix()
 
     for obj in bpy.context.selected_objects:
         objPosition.append(obj.location)
         objDimension.append(obj.dimensions)   
             
     objDistance = distance_vec(min(objPosition), max(objPosition))
-    maxClipping = objDistance + max(max(objDimension))
-    minClipping = min_list_value(objDimension)
+    minClipping = min_list_value(objDimension) / pref.clip_start_factor
+    maxClipping = objDistance + max(max(objDimension)) * pref.clip_end_factor
+    print("min/max/dist: ", minClipping, maxClipping, objDistance, sep=" / ")
 
     for workspace in bpy.data.workspaces:
         #print("workspaces:", workspace.name)
         for screen in workspace.screens:
-            ##print("screen: ", screen.name)
+            #print("screen: ", screen.name)
             for area in screen.areas:
                 if area.type == 'VIEW_3D':
                     #print("area: ", area.type , end='\n')
                     for space in area.spaces:
                         if space.type == 'VIEW_3D':
-                            space.clip_start = minClipping / pref.clip_start_factor
-                            space.clip_end = maxClipping * pref.clip_end_factor
-                            #print("start: ", space.clip_start, "\nend: ", space.clip_end)
+                            #set viewport clipping
+                            space.clip_start = minClipping
+                            space.clip_end = maxClipping
+                            #set camera clipping                            
                             if space.camera and pref.camera_clipping:
                                 #print("camera: ", space.camera.name)
-                                bpy.data.cameras[space.camera.name].clip_start = minClipping / pref.clip_start_factor
-                                bpy.data.cameras[space.camera.name].clip_end = maxClipping * pref.clip_end_factor
-            
+                                bpy.data.cameras[space.camera.name].clip_start = minClipping
+                                bpy.data.cameras[space.camera.name].clip_end = maxClipping
 
-""" def draw_button(self, context):
+
+def draw_button(self, context):
     pref = bpy.context.preferences.addons[__package__.split(".")[0]].preferences    
     
     if pref.button_toggle:
         if context.region.alignment == 'RIGHT':
             layout = self.layout
             row = layout.row(align=True)
-                
+           
             if pref.button_text:
                 row.operator(operator="scene.clipping_assistant", text="Set Clipping", icon='VIEW_CAMERA', emboss=True, depress=False)
             else:
                 row.operator(operator="scene.clipping_assistant", text="", icon='VIEW_CAMERA', emboss=True, depress=False)
-"""
+
+
+def calc_view_matrix():   
+    for window in bpy.context.window_manager.windows:
+        screen = window.screen
+        for area in screen.areas:
+            if area.type == 'VIEW_3D':             
+                view_3d = area.spaces.active.region_3d
+                """ print("window_matrix: ", view_3d.window_matrix)
+                print("view_matrix: ", view_3d.view_matrix) """
+                #print("perspective_matrix: ", view_3d.perspective_matrix)
+                print("view_location: ", view_3d.view_location)
+                print("view_rotation: ", view_3d.view_rotation.to_euler())
+                print("view_distance: ", view_3d.view_distance)
+                
+
 
 class ClippingAssistant_OT_run(Operator):
     bl_idname = "scene.clipping_assistant"
     bl_label = "clipping_assistant"
     bl_description = "Set Start and End Clipping Distance of Camera(s)"
     
-    @classmethod
-    def poll(cls, context):
-        #print("objecs selected", len(context.selected_objects))
-        return context.selected_objects    
-
+    
+    """ @classmethod
+    def poll(cls, context):        
+        ob = context.object
+        return ob and ob.type == 'MESH' """
+        
+    print("TEST")
+    #subscribe_to_obj_loc(bpy.context.object)
+         
+     
     def invoke(self, context, event):
+        print("invoke")
         subscribe_to_obj_loc(bpy.context.object)
         return{'FINISHED'}
+
+    """ 
+    def execute(self, context):
+        print("execute")
+        subscribe_to_obj_loc(bpy.context.object)
+        return{'FINISHED'} """
+       
 
 
 class ClippingAssistantPreferences(AddonPreferences):
@@ -161,7 +194,7 @@ class ClippingAssistantPreferences(AddonPreferences):
         description="When enabled the clipping Distance of the Active Camera is adjusted as well as the Viewport Clip Distance",
         default=False)
     
-    """ button_text: BoolProperty(
+    button_text: BoolProperty(
         name="Show Button Text",
         description="When enabled the Header Button will Show A Text",
         default=True)
@@ -169,13 +202,13 @@ class ClippingAssistantPreferences(AddonPreferences):
     button_toggle: BoolProperty(
         name="Show Button",
         description="When enabled the Header Button will Show",
-        default=True) """
+        default=True)
 
     def draw(self, context):
         layout = self.layout
         layout.use_property_split = True
-        """ layout.prop(self, 'button_toggle') 
-        layout.prop(self, 'button_text')  """
+        layout.prop(self, 'button_toggle') 
+        layout.prop(self, 'button_text')
         layout.prop(self, 'camera_clipping') 
         layout.prop(self, 'clip_start_factor') 
         layout.prop(self, 'clip_end_factor') 
@@ -186,10 +219,8 @@ classes = (
     ClippingAssistantPreferences,
     )
 
-def register():
-    
-    for c in classes:
-        bpy.utils.register_class(c)   
+def register():   
+    [bpy.utils.register_class(c) for c in classes]  
     bpy.types.TOPBAR_HT_upper_bar.prepend(draw_button)
 
 
